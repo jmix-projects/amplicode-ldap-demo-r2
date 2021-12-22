889953f8c5e31f80fedb48210c1bd34ea2e925c3
Denis Ivanov <denis.ivanov@haulmont.com>
2021-12-22 20:03:57 +0400

    Admin credentials envs

--- deployment/dev/backend-docker-runner.sh
+++ deployment/dev/backend-docker-runner.sh
@@ -3,4 +3,4 @@
 cd ../../
 ./gradlew clean build -x test
 cd deployment/dev/
-docker-compose up -f backend-docker-compose.yml
+docker-compose -f backend-docker-compose.yml up
--- src/main/java/com/sample/AmplicodeLdapDemoR2.java
+++ src/main/java/com/sample/AmplicodeLdapDemoR2.java
@@ -1,12 +1,14 @@
 package com.sample;
 
 
+import com.sample.properties.DemoConfigProperties;
 import com.sample.user.management.Role;
 import com.sample.user.management.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.transaction.support.TransactionTemplate;
@@ -14,12 +16,16 @@ import org.springframework.transaction.support.TransactionTemplate;
 import java.util.Collections;
 
 @RequiredArgsConstructor
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class AmplicodeLdapDemoR2 implements CommandLineRunner {
+
+    private static final String LDAP_MANAGER_ROLE = "LDAP_MANAGER";
 
     final UserDetailsManager manager;
     final TransactionTemplate tt;
     final PasswordEncoder passwordEncoder;
+    final DemoConfigProperties demoConfigProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(AmplicodeLdapDemoR2.class, args);
@@ -28,12 +34,14 @@ public class AmplicodeLdapDemoR2 implements CommandLineRunner {
     @Override
     public void run(String... args) {
         tt.executeWithoutResult(status -> {
-            if (!manager.userExists("admin")) {
+            String ldapManagerUsername = demoConfigProperties.getLdapManagerUsername();
+            String ldapManagerPassword = demoConfigProperties.getLdapManagerPassword();
+            if (!manager.userExists(ldapManagerUsername)) {
                 User user = new User();
-                user.setUsername("admin");
-                user.setPassword(passwordEncoder.encode("admin"));
+                user.setUsername(ldapManagerUsername);
+                user.setPassword(passwordEncoder.encode(ldapManagerPassword));
                 Role role = new Role();
-                role.setAuthority("ADMIN");
+                role.setAuthority(LDAP_MANAGER_ROLE);
                 user.setRoles(Collections.singleton(role));
                 manager.createUser(user);
             }
